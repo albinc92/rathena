bd3856fbaa6d36e14e511230ce9e05e41261acbc
Albin Carlson <albin_carlson92@hotmail.com>
2021-01-04 09:29:49 +0100

    This time should work

--- sql-files/wro.sql
+++ sql-files/wro.sql
@@ -44,7 +44,7 @@ UPDATE `item_db2` SET `attack` = `attack` * 2.5 WHERE  `weapon_level` = 4;
 -- Create costume copies of all headgears in item_db
 -- and updates their stats to reflect their nature.
 REPLACE INTO `item_db2`
-SELECT * FROM `item_db` WHERE `location_head_top` = true OR `location_head_mid` = true OR `location_head_mid` = true;
+SELECT * FROM `item_db` WHERE `location_head_top` = true OR `location_head_mid` = true OR `location_head_low` = true;
 
 UPDATE `item_db2`
 SET `id` = `id` + 41000,
@@ -59,9 +59,6 @@ SET `id` = `id` + 41000,
     `job_all` = true,
     `class_all` = true,
     `gender` = 'Both',
-    `location_costume_head_top` = CASE WHEN `location_head_top` = true THEN location_costume_head_top = true END,
-    `location_costume_head_top` = CASE WHEN `location_head_top` = true THEN location_costume_head_top = true END,
-    `location_costume_head_top` = CASE WHEN `location_head_top` = true THEN location_costume_head_top = true END,
     `equip_level_min` = 0,
     `equip_level_max` = 250,
     `refineable` = true,
@@ -77,11 +74,23 @@ SET `id` = `id` + 41000,
     `script` = '',
     `equip_script` = '',
     `unequip_script` = ''
-WHERE `location_head_top` = true OR `location_head_mid` = true OR `location_head_mid` = true;
+WHERE `location_head_top` = true OR `location_head_mid` = true OR `location_head_low` = true;
 
--- UPDATE `item_db2`
--- SET  `location_head_top` = false, `location_head_mid` = false, `location_head_low` = false
--- WHERE `location_head_top` = true OR `location_head_mid` = true OR `location_head_mid` = true;
+UPDATE `item_db2`
+SET  `location_costume_head_top` = true
+WHERE `location_head_top` = true;
+
+UPDATE `item_db2`
+SET  `location_costume_head_mid` = true
+WHERE `location_head_mid` = true;
+
+UPDATE `item_db2`
+SET  `location_costume_head_low` = true
+WHERE `location_head_low` = true;
+
+UPDATE `item_db2`
+SET  `location_head_top` = false, `location_head_mid` = false, `location_head_low` = false
+WHERE `location_head_top` = true OR `location_head_mid` = true OR `location_head_mid` = true;
 
 -- Update scripts of MvP cards
 REPLACE INTO `item_db2` (`id`,`name_aegis`,`name_english`,`type`,`price_buy`,`weight`,`location_shoes`,`flag_buyingstore`,`script`)
