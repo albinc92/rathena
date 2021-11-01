091e5c6de512d44b2f8d3d0ff98c0072c664005e
albinc92 <albin_carlson92@hotmail.com>
2021-11-01 13:18:01 +0100

    BOsses drop more items

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -3756,16 +3756,16 @@ int mob_dead(struct mob_data *md, struct block_list *src, int type)
 
 		// Random equipment drop (WarboundRO)
 		if (sd == mvp_sd) {
-			int loot_count = 1;
+			int loot_count = md->get_bosstype + 1;
 
 			// Mini MVPs drop 2-3 items
 			if(md->get_bosstype == 1) {
-				loot_count = 2 + (rnd() % 1);
+				loot_count = loot_count + (rnd() % loot_count);
 			}
 
 			// MVPs drop 3-5 items
 			if(md->get_bosstype == 2) {
-				loot_count = 3 + (rnd() % 3);
+				loot_count = loot_count + (rnd() % loot_count);
 			}
 
 			for(int i = 0; i < loot_count; i++) {
